332904773554a7f092baf83cc6f4a91e37f2f2cb
nd193 <nd193@student.le.ac.uk>
2022-02-07 15:44:06 +0000

    WIP: Implement scraping of individual job offers #7

--- src/scraping.py
+++ src/scraping.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+import matplotlib.pyplot as plt
 
 #We will be scraping gamesjobdirect.com
 
@@ -32,7 +33,8 @@ def final_page():
     return last_page
     
 
-def scraping():
+def titles():
+    title_list = []
     for page in range(1, int(final_page())+1):
         URL = f"https://www.gamesjobsdirect.com/results?page={page}&stack=0&mt=2&ic=False&l=Leicester&lid=2644668&lat=52.638599395752&lon=-1.13169002532959&r=50&age=0&sper=4"
         page = requests.get(URL)
@@ -41,6 +43,97 @@ def scraping():
         results = soup.find_all("a", class_="job-title")
         #This will need to be changed later so it gets stored in a database instead of printing
         for x in results:
-            print(x["title"])
-        
-        
+            title_list.append(x["title"])
+    return(title_list)
+
+def job_types():
+    soft_engineer = 0
+    data_analyst = 0
+    ui_design = 0
+    video_editor = 0
+    backend_dev = 0
+    producer = 0
+    title_list = titles()
+    for x in title_list:
+        if "data" in x.lower():
+            data_analyst += 1
+        elif "ui" in x.lower():
+            ui_design += 1
+    print(data_analyst)
+    print(ui_design)
+
+def programming_langs():
+    import re
+    python = 0
+    csharp = 0
+    cplus = 0
+    java = 0
+    sql = 0
+    unity = 0
+    for page in range(1, int(final_page())+1):
+        URL = f"https://www.gamesjobsdirect.com/results?page={page}&stack=0&mt=2&ic=False&l=Leicester&lid=2644668&lat=52.638599395752&lon=-1.13169002532959&r=50&age=0&sper=4"
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find_all("a", class_="job-title")
+        for x in results:
+            URL = "https://www.gamesjobsdirect.com" + x["href"]
+            page = requests.get(URL)
+            soup = BeautifulSoup(page.content, "html.parser")
+            result = soup.find("div", class_="post-description")
+            is_python = result.find(string=re.compile('.*{0}.*'.format("Python")))
+            is_csharp = result.find(string=re.compile('.*{0}.*'.format("C#")))
+            is_cplus = result.find(string=re.compile('.*{0}.*'.format("C\+\+")))
+            is_java = result.find(string=re.compile('.*{0}.*'.format("Java")))
+            is_sql = result.find(string=re.compile('.*{0}.*'.format("SQL")))
+            is_unity = result.find(string=re.compile('.*{0}.*'.format("Unity")))
+            if is_python != None:
+                python += 1
+            if is_csharp != None:
+                csharp += 1
+            if is_cplus != None:
+                cplus += 1
+            if is_java != None:
+                java += 1
+            if is_sql != None:
+                sql += 1
+            if is_unity != None:
+                unity += 1
+            
+            continue
+    return [python, csharp, cplus, java, sql, unity]
+             
+def graph():
+    labels = 'Python', 'C#', "C++", "Java", "SQL", "Unity"
+    sizes = programming_langs()
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.show()
+            
+def job_desc():
+    for page in range(1, int(final_page())+1):
+        URL = f"https://www.gamesjobsdirect.com/results?page={page}&stack=0&mt=2&ic=False&l=Leicester&lid=2644668&lat=52.638599395752&lon=-1.13169002532959&r=50&age=0&sper=4"
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find_all("a", class_="job-title")
+        for x in results:
+            URL = "https://www.gamesjobsdirect.com" + x["href"]
+            page = requests.get(URL)
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            job_info = soup.find("div", class_="job-info-container")
+
+            location = job_info.find("label", string="Location").parent.find("p")
+            experience = job_info.find("label", string="Experience Level").parent.find("p")
+            company_name = job_info.find("label", string="Company Name").parent.find("p")
+            job_title = soup.find("h3", class_="margin-b-4")
+            unwanted = job_title.find("span")
+            if unwanted is not None:
+                unwanted.extract()
+            print(job_title.text.strip())
+
+job_desc()
+
