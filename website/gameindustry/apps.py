6cb1907782962292eb5a0d9110bc9cee0500d5ba
acoskun1 <acoskunbiz@gmail.com>
2022-02-08 22:00:31 +0000

    Create homepage url and template folder

---
+++ website/gameindustry/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GameindustryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gameindustry'
--- website/website/urls.py
+++ website/website/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #All requests starting with gameindustry is handled by gameindustry app
+    path('gameindustry/', include('gameindustry.urls'))
 ]
