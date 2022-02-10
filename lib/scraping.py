9c90de3472c0b7cc66b63908ab5b3a7d71ffcdc8
nd193 <nd193@student.le.ac.uk>
2022-02-10 19:48:34 +0000

    Optimise scraping by implementing threading #10

--- lib/scraping.py
+++ lib/scraping.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+from concurrent.futures import ProcessPoolExecutor
 
 
 def django_setup():
@@ -138,61 +139,70 @@ def graph():
     plt.show()
 
 
-def job_desc():
+
+
+def parse(url):
     """
     Scrapes all important info about the jobs and returns lists of all data
     """
     all_offers = []
-    # for page in range(1, int(final_page())+1):
-    for page in range(1, 6):
-        URL = f"https://www.gamesjobsdirect.com/results?page={page}&stack=0&mt=2&ic=False&l=Leicester&lid=2644668&lat=52.638599395752&lon=-1.13169002532959&r=50&age=0&sper=4"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find_all("a", class_="job-title")
+    for x in results:
+        URL = "https://www.gamesjobsdirect.com" + x["href"]
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, "html.parser")
-        results = soup.find_all("a", class_="job-title")
-        for x in results:
-            URL = "https://www.gamesjobsdirect.com" + x["href"]
-            page = requests.get(URL)
-            soup = BeautifulSoup(page.content, "html.parser")
-            # Finds programming Language
-            language = ""
-            job_description = soup.find("div", class_="post-description")
-            is_python = job_description.find(string=re.compile('.*{0}.*'.format("Python")))
-            is_csharp = job_description.find(string=re.compile('.*{0}.*'.format("C#")))
-            is_cplus = job_description.find(string=re.compile('.*{0}.*'.format("C\+\+")))
-            is_java = job_description.find(string=re.compile('.*{0}.*'.format("Java")))
-            is_sql = job_description.find(string=re.compile('.*{0}.*'.format("SQL")))
-            is_unity = job_description.find(string=re.compile('.*{0}.*'.format("Unity")))
-            if is_python != None:
-                language += "Python "
-            if is_csharp != None:
-                language += "C# "
-            if is_cplus != None:
-                language += "C++ "
-            if is_java != None:
-                language += "Java "
-            if is_sql != None:
-                language += "SQL "
-            if is_unity != None:
-                language += "Unity "
+        # Finds programming Language
+        language = ""
+        job_description = soup.find("div", class_="post-description")
+        is_python = job_description.find(string=re.compile('.*{0}.*'.format("Python")))
+        is_csharp = job_description.find(string=re.compile('.*{0}.*'.format("C#")))
+        is_cplus = job_description.find(string=re.compile('.*{0}.*'.format("C\+\+")))
+        is_java = job_description.find(string=re.compile('.*{0}.*'.format("Java")))
+        is_sql = job_description.find(string=re.compile('.*{0}.*'.format("SQL")))
+        is_unity = job_description.find(string=re.compile('.*{0}.*'.format("Unity")))
+        if is_python != None:
+            language += "Python "
+        if is_csharp != None:
+            language += "C# "
+        if is_cplus != None:
+            language += "C++ "
+        if is_java != None:
+            language += "Java "
+        if is_sql != None:
+            language += "SQL "
+        if is_unity != None:
+            language += "Unity "
 
-            job_info = soup.find("div", class_="job-info-container")
-            # Finds job location
-            location = job_info.find("label", string="Location").parent.find("p")
-            # Finds job experience required
-            experience = job_info.find("label", string="Experience Level").parent.find("p")
-            # Finds company name
-            company_name = job_info.find("label", string="Company Name").parent.find("p")
-            # Finds Job Titles
-            job_title = soup.find("h3", class_="margin-b-4")
-            unwanted = job_title.find("span")
-            if unwanted is not None:
-                unwanted.extract()
-            offer = Offer(
-                title=job_title.text.strip(),
-                employer=company_name.text.strip(),
-                location=location.text.strip(),
-                experience=experience.text.strip(),
-                requirements=language
-            )
-            all_offers.append(offer)
+        job_info = soup.find("div", class_="job-info-container")
+        # Finds job location
+        location = job_info.find("label", string="Location").parent.find("p")
+        # Finds job experience required
+        experience = job_info.find("label", string="Experience Level").parent.find("p")
+        # Finds company name
+        company_name = job_info.find("label", string="Company Name").parent.find("p")
+        # Finds Job Titles
+        job_title = soup.find("h3", class_="margin-b-4")
+        unwanted = job_title.find("span")
+        if unwanted is not None:
+            unwanted.extract()
+        offer = Offer(
+            title=job_title.text.strip(),
+            employer=company_name.text.strip(),
+            location=location.text.strip(),
+            experience=experience.text.strip(),
+            requirements=language
+        )
+        all_offers.append(offer)
     Offer.objects.bulk_create(all_offers)
+
+def threads():
+    URLs = []
+    for page in range(1, int(final_page()) + 1):
+        URLs.append(f"https://www.gamesjobsdirect.com/results?page={page}&stack=0&mt=2&ic=False&l=Leicester&lid=2644668&lat=52.638599395752&lon=-1.13169002532959&r=50&age=0&sper=4")
+
+
+    with ProcessPoolExecutor(max_workers=10) as executor:
+        futures = [ executor.submit(parse, url) for url in URLs]
+
