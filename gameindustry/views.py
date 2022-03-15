2b3115834c83be091ea04e0d91fd2ce70f24d092
nd193 <nd193@student.le.ac.uk>
2022-03-15 14:58:52 +0000

    Add Charts for programming languages #12

--- gameindustry/views.py
+++ gameindustry/views.py
@@ -12,8 +12,24 @@ def homepage(request):
     all_offers = Offer.objects.all()
     offerFilter = OfferFilter(request.GET, queryset=all_offers)
     all_offers = offerFilter.qs
+    langs = [0,0,0,0,0,0]
+    for offer in all_offers:
+        if "Python" in offer.requirements.split():
+            langs[0] +=1
+        if "C++" in offer.requirements.split():
+            langs[1] +=1
+        if "C#" in offer.requirements.split():
+            langs[2] +=1
+        if "Java" in offer.requirements.split():
+            langs[3] +=1
+        if "SQL" in offer.requirements.split():
+            langs[4] +=1
+        if "Unity" in offer.requirements.split():
+            langs[5] +=1
     context = {
         'offers': all_offers,
-        'offerFilter': offerFilter
+        'offerFilter': offerFilter,
+        'langs': langs
+
     }
     return render(request, 'homepage.html', context)
